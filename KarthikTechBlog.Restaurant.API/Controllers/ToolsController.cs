20e300542f6613734774c4a1cc495362310aa008
learnsmartcoding <98247322+learnsmartcoding@users.noreply.github.com>
2022-01-28 16:26:58 -0500

    Update ToolsController.cs

--- KarthikTechBlog.Restaurant.API/Controllers/ToolsController.cs
+++ KarthikTechBlog.Restaurant.API/Controllers/ToolsController.cs
@@ -1,45 +1,45 @@
-﻿//using KarthikTechBlog.Restaurant.API.ViewModel.Create;
-//using Microsoft.AspNetCore.Mvc;
-//using Newtonsoft.Json;
-//using System;
-//using System.IO;
-//using System.Security.Cryptography;
-//using System.Text;
-//using System.Threading.Tasks;
+using KarthikTechBlog.Restaurant.API.ViewModel.Create;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace KarthikTechBlog.Restaurant.API.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class ToolsController : ControllerBase
-//    {
+namespace KarthikTechBlog.Restaurant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToolsController : ControllerBase
+    {
 
-//        [HttpPost("SHA256Hash")]
-//        public async Task<IActionResult> GetSHA256Hash([FromBody] EncodeInput model)
-//        {
-//            string requestBody = await new StreamReader(Request.Body).ReadToEndAsync();
-//            dynamic data = JsonConvert.DeserializeObject(requestBody);
-//            string stringToHash = model?.InputString;
+        [HttpPost("SHA256Hash")]
+        public async Task<IActionResult> GetSHA256Hash([FromBody] EncodeInput model)
+        {
+            string requestBody = await new StreamReader(Request.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string stringToHash = model?.InputString;
 
-//            string hashedString = string.Empty;
+            string hashedString = string.Empty;
 
-//            if (string.IsNullOrEmpty(stringToHash))
-//            {
-//                return BadRequest("This HTTP triggered function executed successfully. Pass a stringToHash in the request body to get SHA256 hash response.");
-//            }
+            if (string.IsNullOrEmpty(stringToHash))
+            {
+                return BadRequest("This HTTP triggered function executed successfully. Pass a stringToHash in the request body to get SHA256 hash response.");
+            }
 
-//            using (SHA256 hash = SHA256.Create())
-//            {
-//                var gg = Encoding.UTF8.GetBytes(stringToHash);
-//                var dd = hash.ComputeHash(gg);
+            using (SHA256 hash = SHA256.Create())
+            {
+                var gg = Encoding.UTF8.GetBytes(stringToHash);
+                var dd = hash.ComputeHash(gg);
 
-//                hashedString = Convert.ToBase64String(hash.ComputeHash(Encoding.UTF8.GetBytes(stringToHash)));
-//            }
+                hashedString = Convert.ToBase64String(hash.ComputeHash(Encoding.UTF8.GetBytes(stringToHash)));
+            }
 
-//            string responseMessage = $"your hashed string is => {hashedString}";
+            string responseMessage = $"your hashed string is => {hashedString}";
 
 
-//            return new OkObjectResult(responseMessage);
-//        }
-//    }
-//}
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
